555e720063521a4b7c950e9601340ba3015c6465
Lauro <laurolamas@gmail.com>
2022-09-27 11:41:27 -0700

    Fix 6.2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,15 +7,14 @@ int main(void)
 {
 	int intType;
 	float floatType;
-	double doubleType;
 	char charType;
 	long int longType;
 	long long int longlongType;
 
-	printf("Size of a char: %zu byte\n", sizeof(charType));
-	printf("Size of an int: %zu bytes\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes\n", sizeof(longType));
-	printf("Size of a long long int: %zu bytes\n", sizeof(longlongType));
-	printf("Size of a double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
