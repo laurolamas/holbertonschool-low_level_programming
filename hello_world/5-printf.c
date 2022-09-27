51efe3956c928971f9b270eee010242fbc8b4d29
Lauro <laurolamas@gmail.com>
2022-09-27 10:33:26 -0700

    Added 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	char text[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(text);
+	return (0);
+}
