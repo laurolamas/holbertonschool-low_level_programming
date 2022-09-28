211eefbb720a54ebdb84b1bb7b34b5b10cb6554d
Lauro <laurolamas@gmail.com>
2022-09-28 13:25:17 -0700

    Fix 1.2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * print_alphabet - wefwargwe
  */
@@ -9,18 +9,7 @@ void print_alphabet(void)
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		putchar (ch);
+		_putchar (ch);
 	}
-	putchar ('\n');
-}
-
-/**
- * main - wefwargwe
- * Return: 0
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
+	_putchar ('\n');
 }
