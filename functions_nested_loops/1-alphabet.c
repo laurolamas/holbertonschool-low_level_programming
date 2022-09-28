aaef07d00ec6bf3adb8595e682f50b3e57f2471f
Lauro <laurolamas@gmail.com>
2022-09-28 12:47:39 -0700

    Added README and 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * print_alphabet - wefwargwe
+ */
+
+
+void print_alphabet(void);
+
+/**
+ * main - wefwargwe
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - wefwargwe
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar (ch);
+	}
+	putchar ('\n');
+}
