c3115bea802825c552711fe114e1748d880cae80
Lauro <laurolamas@gmail.com>
2022-09-29 12:12:17 -0700

    Added 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints until 98
+ * @n: initial numer
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n; n > 98; n--)
+			printf("%d, ", n);
+	}
+	else if (n < 98)
+	{
+		for (n; n < 98; n++)
+			printf("%d, ", n);
+	}
+		printf("98\n");
+}
+
