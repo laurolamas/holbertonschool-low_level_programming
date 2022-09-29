1ed02aef18c6669abad420a5fa19989690ea1b6c
Lauro <laurolamas@gmail.com>
2022-09-29 06:41:10 -0700

    Added 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Returnos 1 if c is lowcase, and 0 if else
+ * @c: input number
+ * Return: 1 if lower, 0 if else
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
