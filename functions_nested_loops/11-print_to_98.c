9487a83e2f511780e18b28dc40d0f7de032395ec
Lauro <laurolamas@gmail.com>
2022-09-29 12:15:24 -0700

    Fix 11.1

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,12 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n; n > 98; n--)
+		for (; n > 98; n--)
 			printf("%d, ", n);
 	}
 	else if (n < 98)
 	{
-		for (n; n < 98; n++)
+		for (; n < 98; n++)
 			printf("%d, ", n);
 	}
 		printf("98\n");
