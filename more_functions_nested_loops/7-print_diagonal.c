07d4cc693d13f309affa71553ac0da6e82da947d
Lauro <laurolamas@gmail.com>
2022-10-03 09:01:45 -0700

    fix_7.2

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,13 @@
 void print_diagonal(int n)
 {
 	int j;
+	int k;
 
 	if (n > 0)
 	{
 		for (j = 0; j < n; j++)
 		{
-			for (int k = 0; k < j; k++)
+			for (k = 0; k < j; k++)
 			{
 				_putchar(' ');
 			}
