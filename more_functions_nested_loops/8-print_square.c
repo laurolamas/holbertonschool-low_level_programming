4fcd7892716840f3779cf09be5565d1529105320
Lauro <laurolamas@gmail.com>
2022-10-03 09:10:22 -0700

    Added_8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - Checks if uppercase
+ * @n: Input number
+ */
+void print_diagonal(int n)
+{
+	int j;
+	int k;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (k = 0; k < j; k++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	_putchar('\n');
+}
