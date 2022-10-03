79e90352e9b581c4622892ab2fa9c70c047e2ba4
Lauro <laurolamas@gmail.com>
2022-10-03 07:09:58 -0700

    Added_0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ * _isupper - Checks if uppercase
+ * @c: Input value
+ * Return: 1 if uppercase, 0 if else
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
