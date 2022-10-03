f8463ec3e2f9be931af1a6ca6e38f16e1a7b096b
Lauro <laurolamas@gmail.com>
2022-10-03 08:00:47 -0700

    added_2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Checks if uppercase
+ * @a: Input value
+ * @b: Input value
+ * Return: 1 if uppercase, 0 if else
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
