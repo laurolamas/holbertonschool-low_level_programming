b8bb0741120b2c018270297bfd9991719a29142e
Lauro <laurolamas@gmail.com>
2022-10-06 09:23:24 -0700

    Added_4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - aaaaaaa
+ * @s: aaaaaaa
+ * Return: bbbbbbb
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
