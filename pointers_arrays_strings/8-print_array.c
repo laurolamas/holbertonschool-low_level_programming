81080f598a0fe51a401360de72a3bc2c3ec95689
Lauro <laurolamas@gmail.com>
2022-10-06 12:23:26 -0700

    Added_8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - aaaaaaa
+ * @a: aaaaaaa
+ * @n: nnnnnnn
+ * Return: bbbbbbb
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		_putchar(str[i]);
+		_putchar(',');
+		_putchar(' ');
+	}
+
+	_putchar('\n');
+}
