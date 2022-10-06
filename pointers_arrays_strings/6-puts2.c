c4a0db1f70cf43b182c65a490d1e03c13ff56954
Lauro <laurolamas@gmail.com>
2022-10-06 11:21:56 -0700

    Added_2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts2 - aaaaaaa
+ * @str: aaaaaaa
+ * Return: bbbbbbb
+ */
+
+void _puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i + 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
