f7429f89f86dbdbd1a337a963d2ad39c1af37646
Lauro <laurolamas@gmail.com>
2022-10-06 09:15:59 -0700

    Added_3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - aaaaaaa
+ * @str: aaaaaaa
+ * Return: bbbbbbb
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		putchar(*str[i]);
+
+}
