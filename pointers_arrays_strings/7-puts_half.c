a843efa1d82a82f8ffdfd9e1349105415f59dc30
Lauro <laurolamas@gmail.com>
2022-10-06 12:10:13 -0700

    Fix_7.1

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		continue;
 	len = i;
 
-	for (i = ((len - 1) / 2); (str[i] != '\0'); i++)
+	for (i = (len / 2); (str[i] != '\0'); i++)
 		_putchar(str[i]);
 
 	_putchar('\n');
