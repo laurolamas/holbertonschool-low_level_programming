10070a870ed83b835807cf2f885024fb0b135b5e
Lauro <laurolamas@gmail.com>
2022-10-06 11:45:41 -0700

    Fix6.3

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len;
 
-	for (; str[i] != '\0'; i += 2)
+	for (; s[i] != '\0'; i++)
+		continue;
+	len = i;
+
+	for (i = 0; (str[i] != '\0') && (i < len); i += 2)
 		_putchar(str[i]);
 
 	_putchar('\n');
