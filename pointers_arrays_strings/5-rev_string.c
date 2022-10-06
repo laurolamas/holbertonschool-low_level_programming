26e5559a72ff2bb27d6918d11fc16fd5dd3a656c
Lauro <laurolamas@gmail.com>
2022-10-06 10:47:48 -0700

    Added_5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - aaaaaaa
+ * @s: aaaaaaa
+ * Return: bbbbbbb
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int len;
+	char aux;
+
+	for (; s[i] != '\0'; i++)
+		continue;
+
+	len = i;
+
+	for (i = 0; i <= (len / 2); i++)
+	{
+		aux = (s[i]);
+		s[i] = s[(len - i)];
+		s[(len - i)] = aux
+	}
+
+	_putchar('\n');
+}
