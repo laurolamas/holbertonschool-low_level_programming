7186436e4c88450589321db689bb5054d2756950
Lauro <laurolamas@gmail.com>
2022-10-06 07:37:43 -0700

    Added_1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - aaaaaaa
+ * @a: aaaaaaa
+ * @b: aaaaaaaa
+ * Return: bbbbbbb
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *b;
+
+	*b = *a;
+	*a = c;
+}
