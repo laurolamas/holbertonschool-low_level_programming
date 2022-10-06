30b5a89b9f405a7cec3f593817a04bea2001f181
Lauro <laurolamas@gmail.com>
2022-10-06 12:07:01 -0700

    Added_7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - aaaaaaa
+ * @str: aaaaaaa
+ * Return: bbbbbbb
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int len;
+
+	for (; str[i] != '\0'; i++)
+		continue;
+	len = i;
+
+	for (i = ((len - 1) / 2); (str[i] != '\0'); i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
