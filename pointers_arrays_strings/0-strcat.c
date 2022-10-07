c9b93c363b11fd58f01da3f3795f02c8458b5a96
Lauro <laurolamas@gmail.com>
2022-10-07 06:21:07 -0700

    Added_0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - aaaaaaa
+ *
+ * @dest: dest
+ * @src: src
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int lendest;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		continue;
+	lendest = i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[lendest + i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
