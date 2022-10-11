fa7dc1465adbb028df2614197b3764f4cd0c92d0
Lauro <laurolamas@gmail.com>
2022-10-11 07:30:28 -0700

    Fix_2.1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	int lendest;
 
-	for (i = 0,i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
 //	lendest = i;
