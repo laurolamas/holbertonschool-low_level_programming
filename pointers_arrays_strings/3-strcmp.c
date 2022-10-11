af68fff6209088d8bbd63ddb021bb47b45dcf873
Lauro <laurolamas@gmail.com>
2022-10-11 10:26:43 -0700

    Added_3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - strcmp
+ *
+ * @s1: str1
+ * @s2: str2
+ *
+ * Return: Number
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int ret = 0;
+
+	for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
+		continue;
+
+	ret = s1[(i - 1)] - s2[(i - 1)];
+
+	return (ret);
+}
