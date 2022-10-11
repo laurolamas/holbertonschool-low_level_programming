d8097bef26ac0a404b929bbcb1a5ecf1b8c904a4
Lauro <laurolamas@gmail.com>
2022-10-11 07:29:40 -0700

    Added_2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - aaaaaaa
+ *
+ * @dest: dest
+ * @src: src
+ * @n: nnnnn
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int lendest;
+
+	for (i = 0,i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+//	lendest = i;
+
+//	if (src[0] != '\0')
+//	{
+//		for (i = 0; i < n; i++)
+//			dest[lendest + i] = src[i];
+//	}
+//		return (dest);
+}
