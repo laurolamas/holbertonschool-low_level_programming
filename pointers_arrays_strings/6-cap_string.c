154ba21bdd30242d79eecb63d6acbe3ae165514d
Lauro <laurolamas@gmail.com>
2022-10-12 07:11:35 -0700

    Added_6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - aaaa
+ * @str: str
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	bool isFirst = false;
+
+	char p [13] = {32, 9, 10, ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			for (j = 0; j < 13; j++)
+			{
+				if str[i] = p[j];
+					isFirst = true;
+			}
+			if (isFirst)
+			{
+				str[i] -= 32;
+				isFirst = false;
+			{
+		}
+
+
+	}
+
+	return (str);
+}
