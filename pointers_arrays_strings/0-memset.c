3905e1419355eade8d6680fc1e9348b025d3eacb
Lauro <laurolamas@gmail.com>
2022-10-12 11:27:22 -0700

    Added_0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - memset
+ *
+ * @s: s
+ * @b: b
+ * @n: n
+ *
+ * Return: aaaa
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+
+}
