dd658c4d08ffcd27d03f06cef0b208ff58e9636f
Lauro <laurolamas@gmail.com>
2022-10-12 13:12:15 -0700

    Added_4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - memset
+ *
+ * @s: s
+ * @accept: b
+ *
+ * Return: aaaa
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+		}
+	}
+	return (&s[i]);
+}
