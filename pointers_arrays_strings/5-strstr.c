4353975cf1388b9fa65b3f5d53c5dd0ee96836d5
Lauro <laurolamas@gmail.com>
2022-10-12 13:30:25 -0700

    Fix_5.2

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -15,15 +15,19 @@ char *_strstr(char *haystack, char *needle)
 	int j;
 	int len2 = 0;
 
+	if (needle == '\0')
+		return (&haystack);
+
 	for (j = 0; needle[j] != '\0'; j++)
 		continue;
-	len2= j-1;
-		
+
+	len2 = j - 1;
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-			if (haystack[i] == needle[0])
-				if (haystack[i + len2] == needle[len2])
-					return (&haystack[i]);
+		if (haystack[i] == needle[0])
+			if (haystack[i + len2] == needle[len2])
+				return (&haystack[i]);
 	}
 	return ('\0');
 }
