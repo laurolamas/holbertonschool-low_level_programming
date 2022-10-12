0f05c2a0c527858d4f703a3310e4695f9e5003d3
Lauro <laurolamas@gmail.com>
2022-10-12 11:35:55 -0700

    Added_1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - memset
+ *
+ * @dest: s
+ * @src: b
+ * @n: n
+ *
+ * Return: aaaa
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = src[i];
+	}
+
+	return (dest);
+
+}
