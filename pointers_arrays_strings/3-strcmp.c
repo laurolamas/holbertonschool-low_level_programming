c236012a29467601593d3368fa687aaa0ff4d6b6
Lauro <laurolamas@gmail.com>
2022-10-12 05:42:38 -0700

    Fixx_3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,20 +11,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-/**	int ret = 0;*/
 
 	for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
 		continue;
 
-	if (s1[(i - 1)] > s2[(i - 1)])
-		return (13);
-	else if (s1[(i - 1)] < s2[(i - 1)])
-		return (-13);
-	else
-		return (0);
-/**
- *	ret = s1[(i - 1)] - s2[(i - 1)];
- *
- *	return (ret);
- */
+
+	ret = s1[(i - 1)] - s2[(i - 1)];
+
+	return (ret);
+
 }
