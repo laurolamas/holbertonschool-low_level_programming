4c5e80dae46fb7e569aeae654d657602279f6b26
Lauro <laurolamas@gmail.com>
2022-10-13 08:30:25 -0700

    Fix_8.6

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,10 +17,10 @@ void print_diagsums(int *a, int size)
 	int sumd2;
 
 	for (i = 0; i < size; i++)
-		sumd1 += a[i][i];
+		sumd1 += *a[i][i];
 	
 	for (i = (size-1); i >= 0; i--)
-		sumd2 += a[((size - 1) -i)][i];
+		sumd2 += *a[((size - 1) -i)][i];
 	
 	printf("%d, %d",sumd1,sumd2);
 
