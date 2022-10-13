6d2f1e3e94007fdcdd3371aefd9f8d190f470865
Lauro <laurolamas@gmail.com>
2022-10-13 10:30:48 -0700

    Prueba3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,22 +13,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
+	unsigned int count = 0;
 	int init = 0;
 	int fin = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
+	{
 		for (j = 0; accept[j] != '\0'; j++)
+		{
 			if (s[i] == accept[j] && init == 0)
 				init = i;
 
-	for (j = 0; accept[j] != '\0'; j++)
-                        if (s[0] == accept[j])
-                                init = 0;
-	
-	for (i = init; s[i] != '\0'; i++)
-		for (j = 0; accept[j] != '\0'; j++)
-			if (s[i] != accept[j]  && fin == 0)
+			if (s[i] != accept[j] && init != 0 && fin == 0)
 				fin = i;
-
+		}
+	}
 	return (fin - init);
 }
