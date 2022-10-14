122374fe99784654926062a70f7bf14eeab54b03
Lauro <laurolamas@gmail.com>
2022-10-14 12:50:21 -0700

    Add1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - aaaaa
- *
- * @s: s
- *
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '0'; i++)
-		continue;
-
-	return (i - 1);
-}
-
-/**
  * _print_rev_recursion - aaaaa
  *
  * @s: s
@@ -28,14 +10,9 @@ int _strlen(char *s)
 
 void _print_rev_recursion(char *s)
 {
-	int len;
-
-	len = _strlen(s);
-	s += len;
-	_putchar(*s);
-
-	if (*s-- != '\0')
-		_print_rev_recursion(s--);
+	if (*s)
+		_print_rev_recursion(++s);
+		_putchar(*s);
 	else
 		_putchar('\n');
 
