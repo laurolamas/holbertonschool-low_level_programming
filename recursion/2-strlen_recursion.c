9a95accaa2fb09ae8f04425dfb38376476a47e59
Lauro <laurolamas@gmail.com>
2022-10-14 07:13:58 -0700

    Added_2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - aaaaa
+ *
+ * @s: s
+ *
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s++ != '\0')
+	{
+		len++;
+		_strlen_recursion(s++);
+	}
+	else
+		return (len);
+}
