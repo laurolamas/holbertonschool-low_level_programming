91a71a126058a4559ff3d784c551bcc199024017
Lauro <laurolamas@gmail.com>
2022-10-14 13:35:32 -0700

    Added_4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - aaaa
+ *
+ * @x: x
+ * @y: y
+ *
+ * Return: pow
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return ((x * _pow_recursion(x, (y - 1))));
+
+	}
+
+	else if (y == 0)
+		return (1);
+	else
+		return (-1);
+}
