71e29bae76e1b0f037f5fbddb981d8bfe9bcb456
Lauro <laurolamas@gmail.com>
2022-10-14 06:54:21 -0700

    Added_0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - aaaaa
+ *
+ * @s: s
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+}
