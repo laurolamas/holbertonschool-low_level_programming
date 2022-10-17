c24b51d949391c8f3282cea9b35ccb5d86c335b2
Lauro <laurolamas@gmail.com>
2022-10-17 11:56:39 -0700

    Added_1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
