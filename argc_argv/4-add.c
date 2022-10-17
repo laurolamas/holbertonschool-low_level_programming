c407c270987c05308369d818276c058b1256c91b
Lauro <laurolamas@gmail.com>
2022-10-17 13:31:35 -0700

    Fix_4.1

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,14 +14,16 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum = 0;
+	int j;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (strtol(argv[i], NULL, 10) == 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 
 		sum += strtol(argv[i], NULL, 10);
 	}
