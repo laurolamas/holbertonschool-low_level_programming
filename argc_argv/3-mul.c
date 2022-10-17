f260ce62fa8f12a078c83aa9f68946ea2a0d48be
Lauro <laurolamas@gmail.com>
2022-10-17 12:21:54 -0700

    Added_3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 0;
+	int y = 0;
+	int mul = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = strtol(argv[1], NULL, 10);
+	y = strtol(argv[2], NULL, 10);
+	mul = x * y;
+	printf("%d\n", mul);
+	return (0);
+}
