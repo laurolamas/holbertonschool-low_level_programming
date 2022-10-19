e51a466b26a8fd9d099070db03ea365d5eb6223e
Lauro <laurolamas@gmail.com>
2022-10-19 13:14:02 -0700

    Strlen

---
+++ malloc_free/_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - aaaaaaa
+ * @s: aaaaaaa
+ * Return: bbbbbbb
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		continue;
+
+	return	(i);
+}
