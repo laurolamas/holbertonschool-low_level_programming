8e969e221fcf23b6ae25d5803263436457732f18
Lauro <laurolamas@gmail.com>
2022-10-22 17:13:50 -0700

    Added_2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - calloc
+ *
+ * @nmemb: nmemb
+ * @size:
+ *
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = NULL;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (!ptr)
+		return (NULL);
+
+	return (ptr);
+}
