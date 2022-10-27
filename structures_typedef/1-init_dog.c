e4209fa36f82f1dcf9934543fbab2d1fef7243d1
Lauro <laurolamas@gmail.com>
2022-10-27 13:27:07 -0700

    Fix_1.1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,8 +13,11 @@
  * Return: void
  */
 
-void init_dog(struct dog*d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!d)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
