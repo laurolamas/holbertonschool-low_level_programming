97c7f2a7651757842cde117ec5f21813b9cd25c0
Lauro <laurolamas@gmail.com>
2022-10-27 17:10:48 -0700

    upd_dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,16 +10,16 @@
  * Description: Longer description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *);*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *);
 
 #endif
