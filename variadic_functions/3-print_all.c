a792625b0465c62550a5e4faf01c31af0377605d
Lauro <laurolamas@gmail.com>
2022-11-01 12:50:08 -0700

    Added_3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+/**
+ * print_all - Print all
+ *
+ * @n: n
+ * @separator: s
+ * Return: n
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	int i = 0;
+	bool printedSomething = false;
+
+	va_start(ptr, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ptr, int));
+				printedSomething = true;
+				break;
+			case 'i':
+				printf("%d", va_arg(ptr, int));
+				printedSomething = true;
+				break;
+			case 'f':
+				printf("%f", va_arg(ptr, double));
+				printedSomething = true;
+				break;
+			case 's':
+				printf("%s", va_arg(ptr, char *));
+				printedSomething = true;
+				break;
+		}
+		if (format[i + 1] && printedSomething)
+		{
+			printf(", ");
+			printedSomething = false;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
