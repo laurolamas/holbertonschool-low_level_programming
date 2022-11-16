04ba12cb647f520e33eec59dcae15ff0c48b552f
Lauro <laurolamas@gmail.com>
2022-11-16 11:26:06 -0800

    Fix_9.1

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prevnode = NULL;
 	listint_t *newnode = NULL;
 
-	if (!head || !*head)
+	if (!head)
 		return (NULL);
 
 	for (i = 0; i < idx; i++)
@@ -34,7 +34,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode->n = n;
 	newnode->next = ptr;
-	prevnode->next = newnode;
+	if (idx && *head)
+		prevnode->next = newnode;
+	else
+		*head = newnode;
 
 	return (newnode);
 }
