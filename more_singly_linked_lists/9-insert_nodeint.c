564c30a53fdce831a462670c07a9332ba7e9353a
Lauro <laurolamas@gmail.com>
2022-11-16 10:53:28 -0800

    Added_9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - Insert
+ * @head: head
+ * @idx: index
+ * @n: n
+ * Return: return
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *ptr = *head;
+	listint_t *prevnode = NULL;
+	listint_t *newnode = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+
+	for (i = 0; i < idx; i++)
+	{
+		if (i == (idx - 1))
+			prevnode = ptr;
+
+		if (ptr->next)
+			ptr = ptr->next;
+		else
+			return (NULL);
+	}
+
+	newnode = malloc(sizeof(listint_t));
+
+	if (!newnode)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = ptr;
+	prevnode->next = newnode;
+
+	return (newnode);
+}
