34eeae255aba55d88ffc9e8cb33e0b6e70f38593
Lauro <laurolamas@gmail.com>
2022-11-20 20:15:40 -0300

    Added_3_4

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = ~(1 << index);
+
+	if ((*n & mask) | (1 << index))
+	{
+		*n = (*n & mask) | (1 << index);
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = ~(1 << index);
+	*n = *n & mask;
+	return (1);
+}
