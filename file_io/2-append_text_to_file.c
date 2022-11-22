18629b221967639540d7cb6bb5218e6355f33ac1
Lauro <laurolamas@gmail.com>
2022-11-22 11:57:39 -0800

    Fix_2.1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,12 +16,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
+
+	if (!text_content)
+		return (-1);
 
 	sz = write(fd, text_content, strlen(text_content));
 
 	if (sz == -1)
-		return (0);
+		return (-1);
 
 	close(fd);
 
