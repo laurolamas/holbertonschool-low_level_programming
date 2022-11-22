f0c9f52ccbc5c181af9522085b8eb0e6905d2dfa
Lauro <laurolamas@gmail.com>
2022-11-22 06:01:14 -0800

    fix_1.2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,22 +1,26 @@
 #include "main.h"
 #include <string.h>
 /**
+ * create_file - Create file
+ * @filename: filename
+ * @text_content: text content
+ * Return: 1 on succes
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd, sz;
+	int fd, sz;
 
-        fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
-        if (fd == -1)
-                return (0);
+	if (fd == -1)
+		return (0);
 
-        sz = write(fd, text_content, strlen(text_content));
+	sz = write(fd, text_content, strlen(text_content));
 
-		if (sz == -1)
-			return (0);
+	if (sz == -1)
+		return (0);
 
-        close(fd);
+	close(fd);
 
-        return (1);
+	return (1);
 }
