cd6407ee4456fd7c24433e4bf528d1014cc0fa7e
Lauro <laurolamas@gmail.com>
2022-11-21 21:39:32 -0300

    Added_0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - Read
+ * @filename: filename
+ * @letters: letters
+ * Return: sixe_t
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int ret;
+	int readd;
+	char *buffer = NULL;
+
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1 || !filename)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+		return (0);
+
+	readd = read(fd, buffer, letters);
+
+	if (readd == -1)
+		return (0);
+
+	ret = write(1, buffer, readd);
+
+
+	free(buffer);
+
+	close(fd);
+
+	return (ret);
+
+}
