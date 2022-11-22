0d80fee78e362c7d6d84ab15a20b66b2742fae0d
Lauro <laurolamas@gmail.com>
2022-11-22 11:52:13 -0800

    Fix_1.1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,15 +10,15 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, sz;
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	sz = write(fd, text_content, strlen(text_content));
 
 	if (sz == -1)
-		return (0);
+		return (-1);
 
 	close(fd);
 
