69ff074ea5f442252d640d45c30f9fe3b7425d0d
Lauro <laurolamas@gmail.com>
2022-11-22 11:55:11 -0800

    Fix_1.3

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return(1);
+		return (1);
 
 	sz = write(fd, text_content, strlen(text_content));
 
