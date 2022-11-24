45c4bcf06b9f9ab1fb800f8b0bbf217554e10d22
Lauro <laurolamas@gmail.com>
2022-11-24 17:28:25 -0300

    Fix_8.1

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,46 +1,42 @@
 #include "lists.h"
+
 /**
  * delete_dnodeint_at_index - delete
  * @head: head
  * @index: index
  * Return: return
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
 	dlistint_t *ptr = *head;
-	dlistint_t *prevnode = NULL;
 
-	if (!head || !*head)
+	if (!head || !(*head))
 		return (-1);
 
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		if ((*head)->prev)
+			(*head)->prev = NULL;
 		free(ptr);
+		if (!((*head)->next))
+			*head = NULL;
 		return (1);
 	}
 
 	for (i = 0; i < index; i++)
 	{
-		if (i == (index - 1))
-			prevnode = ptr;
-
 		if (ptr->next)
 			ptr = ptr->next;
 		else
 			return (-1);
 	}
-	if (!(ptr->next))
-	{
-		prevnode->next = NULL;
-		free(ptr);
-		return (1);
-	}
-	else
-	{
-		prevnode->next = ptr->next;
-		free(ptr);
-		return (1);
-	}
+
+	(ptr->prev)->next = ptr->next;
+	if (ptr->next)
+		(ptr->next)->prev = ptr->prev;
+	free(ptr);
+	return (1);
 }
