33f0d93516dea34944e634a4c38d530deee0a8af
Lauro <laurolamas@gmail.com>
2022-11-24 14:40:42 -0300

    Fix_2.3

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,10 +19,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	node->n = n;
 
 	node->next = *head;
-	
+
 	node->prev = NULL;
 
-	(*head)->prev = node;
+	if (*head)
+		(*head)->prev = node;
 
 	*head = node;
 
