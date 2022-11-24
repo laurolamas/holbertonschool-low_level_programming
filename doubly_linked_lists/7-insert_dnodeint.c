1c25f9362c83e5b4c826c102d406ce86177c05b8
Lauro <laurolamas@gmail.com>
2022-11-24 15:12:54 -0300

    Fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if ((len(*h)) == idx)
 		return (add_dnodeint_end(h, n));
-	if ((len(*h)) == 0)
+	if (idx == 0)
 		return (add_dnodeint_end(h, n));
 
 	for (i = 0; i < idx; i++)
