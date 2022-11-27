f922702e83d0bee088161da351ae2afbb0e0a813
Lauro <laurolamas@gmail.com>
2022-11-27 16:36:44 -0300

    Added_4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - Get value of key
+ * @ht: ht
+ * @key: key
+ * Return: Value
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (((ht->array)[i]) && (strcmp(((ht->array)[i])->key, key) == 0))
+			return ((ht->array)[i]->value);
+	}
+	return (NULL);
+}
