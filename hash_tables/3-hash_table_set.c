342f6b8dc23773984ce23a7dada5f8562e73e18e
Lauro <laurolamas@gmail.com>
2022-11-27 15:22:01 -0300

    Fix_3.2

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (((ht->array)[index]) != NULL)
 		index = 0;
-	else 
+	else
 	{
 		((ht->array)[index]) = malloc(sizeof(hash_node_t));
 
