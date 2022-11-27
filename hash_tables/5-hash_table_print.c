820dd638f4840e6bbfbd4ca1e70b69de35d2f0e7
Lauro <laurolamas@gmail.com>
2022-11-27 18:43:54 -0300

    Added_5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,6 +1,27 @@
 #include "hash_tables.h"
 
 /**
+ * get_last_element - get
+ * @ht: ht
+ * Return: last
+ */
+
+unsigned long int get_last_element(const hash_table_t *ht)
+{
+	unsigned long int i;
+	unsigned long int count;
+
+
+	for (i = 0 ; i < ht->size; i++)
+	{
+		if (ht->array[i])
+			count = i;
+	}
+
+	return (count);
+}
+
+/**
  * hash_table_print - hash_table_print
  * @ht: ht
  */
@@ -9,20 +30,20 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *p;
-	char n;
+	unsigned long int last;
+
+	last = get_last_element(ht);
 
 	printf("{");
 	for (i = 0; ht && i < ht->size; i++)
 	{
 		p = ht->array[i];
-		n = 0;
 		while (p)
 		{
-			if (n)
-				printf(", ");
 			printf("'%s': '%s'", p->key, p->value);
+			if (i != last)
+				printf(", ");
 			p = p->next;
-			n = 1;
 		}
 	}
 	printf("}\n");
