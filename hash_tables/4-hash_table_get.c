8779682504d0a1f41132a12b8f1ce14c97702a3a
Lauro <laurolamas@gmail.com>
2022-11-27 18:11:51 -0300

    Fix_4.1

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
 
+	if (!ht || !key)
+		return (NULL);
+
 	for (i = 0; i < ht->size; i++)
 	{
 		if (((ht->array)[i]) && (strcmp(((ht->array)[i])->key, key) == 0))
